184e1b6bf8cb6f53f4f6fa90ff293ea3c61f4256
cti1650 <cti1650@gmail.com>
2022-12-26 00:28:49 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Bookmarklet ChatGPT Tools
+
+## 目的
+
+ChatGPTの出力結果を効率よくアウトプットしたいという目的で作成
---
+++ public/marklet.js
@@ -0,0 +1,68 @@
+window.mklet_chatgpt_tools = (d, slackToken, slackChannel) => {
+  const copyText = (text) => {
+    if (!text) return;
+    const textArea = document.createElement("textarea");
+    textArea.value = text;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textArea);
+  };
+  const sendToSlack = (slackToken, slackChannel, text) => {
+    // 投稿する内容を設定する
+    const message = {
+      channel: slackChannel,
+      text: text
+    };
+    const formData = new FormData();
+    Object.keys(message).forEach(key=>{
+      formData.append(key, message[key]);
+    })
+
+    // Slack API のエンドポイントを指定する
+    const endpoint = "https://slack.com/api/chat.postMessage";
+
+    fetch(endpoint,{
+      method:"post",
+      mode:'no-cors',
+      headers:{
+        "Authorization":`Bearer ${slackToken}`
+      },
+      body:formData
+    })
+  };
+  const parseChatGPT = () => {
+    const resultsElements = [
+      ...document.querySelectorAll(
+        "[class*=react-scroll-to-bottom] > .flex > div.border-b.bg-gray-50"
+      ),
+    ];
+    const lastResultElement = resultsElements[resultsElements.length - 1];
+    if (!lastResultElement) return null;
+    const lastRequestElement = lastResultElement.previousElementSibling;
+    const lastResult = [
+      ...lastResultElement.querySelector(".markdown").childNodes,
+    ]
+      .map((item) => {
+        if (item.querySelector("pre code")) {
+          return '```' + `\n${item.textContent}\n` + '```';
+        }
+        return item.textContent;
+      })
+      .join("\n");
+    const lastRequest = lastRequestElement.textContent;
+    const data = {
+      lastRequest,
+      lastResult,
+      text: `${lastRequest}\n${lastResult}`,
+    };
+    return data;
+  };
+  const data = parseChatGPT();
+  if (slackToken && slackChannel) {
+    sendToSlack(slackToken,slackChannel,data?.text)
+  } else {
+    copyText(data?.text);
+    alert("クリップボードに保存しました: " + data?.text);
+  }
+};
